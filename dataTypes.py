5f97a12e51584f2276ffd4946dd5a07a1db65052
nitinn5 <64216093+nitinn5@users.noreply.github.com>
2021-01-16 10:30:14 +0530

    Update dataTypes.py

--- dataTypes.py
+++ dataTypes.py
@@ -397,7 +397,334 @@ for item in datetimes_list[:10]:
 
 ######################### Dictionaries, ex.19 ################################################
 
+"""
+Pieces of Time
+When working with datetime objects, you'll often want to group them by some component of the datetime such as the month, year, day, etc. Each of these are available as attributes on an instance of a datetime object.
 
+You're going to work with the summary of the CTA's daily ridership. It contains the following columns, in order: service_date, day_type, bus, rail_boardings, and total_rides. The modules defaultdict and datetime have already been imported for you.
+"""
+# Create a defaultdict of an integer: monthly_total_rides
+monthly_total_rides = defaultdict(int)
+
+# Loop over the list daily_summaries
+for daily_summary in daily_summaries:
+    # Convert the service_date to a datetime object
+    service_datetime = datetime.strptime(daily_summary[0],'%m/%d/%Y')
+
+    # Add the total rides to the current amount for the month
+    monthly_total_rides[service_datetime.month] += int(daily_summary[4]) # Be careful! month is not a method but attribute for the datetime instance. Don't use ()
+    
+# Print monthly_total_rides
+print(monthly_total_rides)
+
+
+######################### Dictionaries, ex.20 ################################################
+
+"""
+Creating DateTime Objects... Now
+Often when working with datetime objects, you'll want to work on windows or ranges that start from the current date and time. You can do this using the datetime now functions. There is a .now() method on the datetime object in the datetime module and a .utcnow() method. The .now() method returns the current local time on the machine on which it is run, and .utcnow() does the same thing but returns the value in UTC time. You'll need to be very familiar with these methods.Creating DateTime Objects... Now
+Often when working with datetime objects, you'll want to work on windows or ranges that start from the current date and time. You can do this using the datetime now functions. There is a .now() method on the datetime object in the datetime module and a .utcnow() method. The .now() method returns the current local time on the machine on which it is run, and .utcnow() does the same thing but returns the value in UTC time. You'll need to be very familiar with these methods.
+"""
+# Import datetime from the datetime module
+from datetime import datetime
+
+# Compute the local datetime: local_dt
+local_dt = datetime.now()
+
+# Print the local datetime
+print(local_dt)
+
+# Compute the UTC datetime: utc_dt
+utc_dt = datetime.utcnow()
+
+# Print the UTC datetime
+print(utc_dt)
+
+######################### Dictionaries, ex.21 ################################################
+
+"""
+Timezones
+In order to work effectively with other timezones, you can use the pytz library. To use timezones, you need to import the timezone object from the pytz module. Then you can use the timezone constructor and pass it a name of a timezone, such as CT = timezone('US/Central'). You can get a full list of timezone names at Wikipedia. In Python 3, you can make a datetime object "aware" by passing a timezone as the tzinfo keyword argument to the .replace() method on a datetime instance.
+
+An "aware" datetime object has an .astimezone() method that accepts a timezone object and returns a new datetime object in the desired timezone. If the tzinfo is not set for the datetime object it assumes the timezone of the computer you are working on.
+"""
+# Create a Timezone object for Chicago
+chicago_usa_tz = timezone('US/Central')
+
+# Create a Timezone object for New York
+ny_usa_tz = timezone('US/Eastern')
+
+# Iterate over the daily_summaries list
+for orig_dt, ridership in daily_summaries:
+
+    # Make the orig_dt timezone "aware" for Chicago
+    chicago_dt = orig_dt.replace(tzinfo=chicago_usa_tz)
+    
+    # Convert chicago_dt to the New York Timezone
+    ny_dt = chicago_dt.astimezone(ny_usa_tz)
+    
+    # Print the chicago_dt, ny_dt, and ridership
+    print('Chicago: %s, NY: %s, Ridership: %s' % (chicago_dt, ny_dt, ridership))
+
+######################### Dictionaries, ex.22 ################################################
+
+"""
+inding a time in the future and from the past
+Another common case when working with times is to get a date 30, 60, or 90 days in the past from some date. In Python, the timedelta object from the datetime module is used to represent differences in datetime objects. You can create a timedelta by passing any number of keyword arguments such as days, seconds, microseconds, milliseconds, minutes, hours, and weeks to timedelta().
+
+Once you have a timedelta object, you can add or subtract it from a datetime object to get a datetime object relative to the original datetime object.
+"""
+# Import timedelta from the datetime module
+from datetime import timedelta
+
+# Build a timedelta of 30 days: glanceback
+glanceback = timedelta(days=30)
+
+# Iterate over the review_dates as date
+for date in review_dates:
+    # Calculate the date 30 days back: prior_period_dt
+    prior_period_dt = date - glanceback
+    
+    # Print the review_date, day_type and total_ridership
+    print('Date: %s, Type: %s, Total Ridership: %s' %
+         (date,
+          daily_summaries[date]['day_type'],
+          daily_summaries[date]['total_ridership']))
+
+    # Print the prior_period_dt, day_type and total_ridership
+    print('Date: %s, Type: %s, Total Ridership: %s' %
+          (prior_period_dt, 
+          daily_summaries[prior_period_dt]['day_type'], 
+          daily_summaries[prior_period_dt]['total_ridership']))
+
+######################### Dictionaries, ex.22 ################################################
+
+"""
+Finding differences in DateTimes
+Just like you were able to subtract a timedelta from a datetime to find a date in the past, you can also calculate the difference between two dates to get the timedelta between in return. Here, you'll find out how much time has elapsed between two transit dates.
+"""
+
+# Iterate over the date_ranges
+for start_date, end_date in date_ranges:
+    # Print the End and Start Date
+    print(end_date, start_date)
+    # Print the difference between each end and start date
+    print(end_date - start_date)
+
+
+######################### Dictionaries, ex.23 ################################################
+
+"""
+Localizing time with pendulum
+Here, you're going to use pendulum to practice doing some common datetime operations!
+"""
+# Import the pendulum module
+import pendulum
+# Create a now datetime for Tokyo: tokyo_dt
+
+tokyo_dt = pendulum.now( tz = 'Asia/Tokyo')
+
+# Covert the tokyo_dt to Los Angeles: la_dt
+la_dt = tokyo_dt.in_timezone(tz = 'America/Los_Angeles')
+
+# Print the ISO 8601 string of la_dt
+print(la_dt.to_iso8601_string())
+
+
+######################### Dictionaries, ex.24 ################################################
+
+"""
+Humanizing Differences with Pendulum
+Pendulum provides a powerful way to convert strings to pendulum datetime objects via the .parse() method. Just pass it a date string and it will attempt to convert into a valid pendulum datetime. By default, .parse() can process dates in ISO 8601 format. To allow it to parse other date formats, pass strict = False.
+
+It also has a wonderful alternative to timedelta. When calculating the difference between two dates by subtraction, pendulum provides methods such as .in_days() to output the difference in a chosen metric. These are just the beginning of what pendulum can do for you.
+
+visit link: https://pendulum.eustace.io/docs/ 
+"""
+
+# Iterate over date_ranges
+for start_date, end_date in date_ranges:
+
+    # Convert the start_date string to a pendulum date: start_dt 
+    start_dt = pendulum.parse(start_date, strict = False)
+    
+    # Convert the end_date string to a pendulum date: end_dt 
+    end_dt = pendulum.parse(end_date, strict = False)
+    
+    # Print the End and Start Date
+    print(end_dt, start_dt)
+    
+    # Calculate the difference between end_dt and start_dt: diff_period
+    diff_period = end_dt - start_dt
+    
+    # Print the difference in days
+    print(diff_period.in_days())
+
+######################### Data Types for Data Science in Python: Project on Chicago Crime Data ################################################
+
+######################### Task 1 ################################################
+#Your job in this exercise is to use a CSV Reader to load up a list to hold the data you're going to analyze.
+
+# Import the csv module
+import csv
+
+# Create the file object: csvfile
+csvfile = open('crime_sampler.csv','r')
+
+# Create an empty list: crime_data
+crime_data = []
+
+# Loop over a csv reader on the file object
+for row in csv.reader(csvfile):
+
+    # Append the date, type of crime, location description, and arrest
+    crime_data.append((row[0], row[2], row[4], row[5]))
+    
+# Remove the first element from crime_data
+crime_data.pop(0)
+
+# Print the first 10 records
+print(crime_data[:10])
+
+######################### Task 2 ################################################
+# Find the Months with the Highest Number of Crimes
+# Using the crime_data list from the prior exercise, you'll answer a common question that arises when dealing with crime data: How many crimes are committed each month?
+
+# Import necessary modules
+from collections import Counter
+from datetime import datetime
+
+
+# Create a Counter Object: crimes_by_month
+crimes_by_month = Counter()
+
+# Loop over the crime_data list
+for item in crime_data:
+    
+    # Convert the first element of each item into a Python Datetime Object: date
+    date = datetime.strptime(item[0], '%m/%d/%Y %I:%M:%S %p')
+    
+    # Increment the counter for the month of the row by one
+    crimes_by_month[date.month] += 1
+    
+# Print the 3 most common months for crime
+print(crimes_by_month.most_common(3))
+
+######################### Task 3 ################################################
+"""
+Transforming your Data Containers to Month and Location
+Now let's flip your crime_data list into a dictionary keyed by month with a list of location values for each month, and filter down to the records for the year 2016. Remember you can use the shell to look at the crime_data list, such as crime_data[1][4] to see the location of the crime in the second item of the list (since lists start at 0).
+"""
+# Import necessary modules
+from collections import defaultdict
+from datetime import datetime
+
+# Create a dictionary that defaults to a list: locations_by_month
+locations_by_month = defaultdict(list)
+
+# Loop over the crime_data list
+for row in crime_data:
+    # Convert the first element to a date object
+    date = datetime.strptime(row[0], '%m/%d/%Y %I:%M:%S %p')
+    
+    # If the year is 2016 
+    if date.year == 2016:
+        # Set the dictionary key to the month and append the location (fifth element) to the values list
+        locations_by_month[date.month].append(row[4])
+    
+# Print the dictionary
+print(locations_by_month)
+
+######################### Task 4 ################################################
+"""
+Find the Most Common Crimes by Location Type by Month in 2016
+Using the locations_by_month dictionary from the prior exercise, you'll now determine common crimes by month and location type. Because your dataset is so large, it's a good idea to use Counter to look at an aspect of it in an easier to manageable size and learn more about it.
+"""
+# Import Counter from collections
+from collections import Counter
+
+# Loop over the items from locations_by_month using tuple expansion of the month and locations
+for month, locations in locations_by_month.items():
+    # Make a Counter of the locations
+    location_count = Counter(locations)
+    # Print the month 
+    print(month)
+    # Print the most common location
+    print(location_count.most_common(5))
+
+######################### Task 5 ################################################
+"""
+Reading your Data with DictReader and Establishing your Data Containers
+Your data file, crime_sampler.csv contains in positional order: the date, block where it occurred, primary type of the crime, description of the crime, description of the location, if an arrest was made, was it a domestic case, and city district.
+"""
+
+# Create the CSV file: csvfile
+csvfile = open('crime_sampler.csv','r')
+
+# Create a dictionary that defaults to a list: crimes_by_district
+crimes_by_district = defaultdict(list)
+
+# Loop over a DictReader of the CSV file
+for row in csv.DictReader(csvfile):
+
+    # Pop the district from each row: district
+    district = row.pop('District')
+
+    # Append the rest of the data to the list for proper district in crimes_by_district
+    crimes_by_district[district].append(row)
+
+######################### Task 6 ################################################    
+    
+"""
+Determine the Arrests by District by Year
+Using your crimes_by_district dictionary from the previous exercise, you'll now determine the number arrests in each City district for each year. Counter is already imported for you. You'll want to use the IPython Shell to explore the crimes_by_district dictionary to determine how to check if an arrest was made.
+"""
+# Loop over the crimes_by_district using expansion as district and crimes
+for district, crimes in crimes_by_district.items():
+    # Print the district
+    print(district)
+    
+    # Create an empty Counter object: year_count
+    year_count = Counter()
+    
+    # Loop over the crimes:
+    for crime in crimes:
+         # If there was an arrest
+        if crime['Arrest'] == 'true':
+            # Convert the Date to a datetime and get the year
+            year = datetime.strptime(crime['Date'], '%m/%d/%Y %I:%M:%S %p').year
+            # Increment the Counter for the year
+            year_count[year] += 1
+            
+    # Print the counter
+    print(year_count)
+
+
+######################### Task 7 ################################################
+
+"""
+Unique Crimes by City Block
+You're in the home stretch!
+
+Here, your data has been reshaped into a dictionary called crimes_by_block in which crimes are listed by city block. Your task in this exercise is to get a unique list of crimes that have occurred on a couple of the blocks that have been selected for you to learn more about. You might remember that you used set() to solve problems like this in Chapter 1.
+"""
+
+# Create a unique list of crimes for the first block: n_state_st_crimes
+n_state_st_crimes = set(crimes_by_block['001XX N STATE ST'])
+
+# Print the list
+print(n_state_st_crimes)
+
+# Create a unique list of crimes for the second block: w_terminal_st_crimes
+w_terminal_st_crimes = set(crimes_by_block['0000X W TERMINAL ST'])
+
+# Print the list
+print(w_terminal_st_crimes)
+
+# Find the differences between the two blocks: crime_differences
+crime_differences = n_state_st_crimes - w_terminal_st_crimes
+# Print the differences
+print(crime_differences)
 
 
 
