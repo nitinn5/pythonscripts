72e6037fa82bd3bd33da456085ca75c3475bdd72
nitinn5 <64216093+nitinn5@users.noreply.github.com>
2021-01-19 21:56:30 +0530

    Update efficientPythonCodeExamples.py

--- efficientPythonCodeExamples.py
+++ efficientPythonCodeExamples.py
@@ -1,3 +1,5 @@
+#:::::::::::::  Built-in practice: enumerate() :::::::::::::::::::::::::::::::::::::::::::
+
 # Rewrite the for loop to use enumerate
 indexed_names = []
 for i, name in enumerate(names):
@@ -12,3 +14,39 @@ print(indexed_names_comp)
 # Unpack an enumerate object with a starting index of one
 indexed_names_unpack = [*enumerate(names, 1)]
 print(indexed_names_unpack)
+
+
+#:::::::::::::  Built-in practice: map()      :::::::::::::::::::::::::::::::::::::::::::
+
+# Use map to apply str.upper to each element in names
+names_map  = map(str.upper,names)
+
+# Print the type of the names_map
+print(type(names_map))
+
+# Unpack names_map into a list
+names_uppercase = [*names_map]
+
+# Print the list created above
+print(names_uppercase)
+
+#:::::::::::::  Basic NumPy indexing and operations     :::::::::::::::::::::::::::::::::
+
+# Print second row of nums
+print(nums[1,:])
+
+# Print all elements of nums that are greater than six
+print(nums[nums>6])
+
+# Double every element of nums
+nums_dbl = nums*2
+print(nums_dbl)
+
+# Replace the third column of nums
+nums[:,2] = nums[:,2] + 1
+print(nums)
+
+
+
+
+
